4c33bcaf60a2f4763802eb7dceeeeabd51373480
tidaratkhanma <32787554+tidaratkhanma@users.noreply.github.com>
2018-05-09 17:18:08 +0700

    01-todos-frontend

---
+++ src/components/TodosFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column ">
+        <p class="is-pulled-left"> {{count}} items left</p>
+        <visibility-input/>
+      </div>
+    </div>
+    <div class="columns is-centered">
+      <div class="column is-4">
+      </div>
+    </div>
+    <center><button class="button" @click="clearCompeleted()">Clear ....................</button></center>
+  </div>
+</template>
+
+<script>
+import VisibilityInput from '@/components/VisibilityInput'
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  components: {
+    VisibilityInput
+  },
+  computed: {
+    ...mapGetters(['count', 'countActives', 'countCompleted'])
+  },
+  methods: {
+    ...mapActions(['clearCompeleted'])
+  }
+}
+</script>
